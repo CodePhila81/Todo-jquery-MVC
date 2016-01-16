d68d2f304ac1ead038ce5822239b015850dc20b9
Cyrus <dropbox.music78@gmail.com>
2016-01-16 10:06:33 -0500

    First commit

---
+++ js/app.js
@@ -0,0 +1,102 @@
+jQuery(function ($) {
+	'use strict';
+
+	Handlebars.registerHelper('eq', function (a, b, options) {
+		return a === b ? options.fn(this) : options.inverse(this);
+	});
+
+	var ENTER_KEY = 13;
+	var ESCAPE_KEY = 27;
+
+	var util = {
+		uuid: function () {
+			/*jshint bitwise:false */
+			var i, random;
+			var uuid = '';
+
+			for (i = 0; i < 32; i++) {
+				random = Math.random() * 16 | 0;
+				if (i === 8 || i === 12 || i === 16 || i === 20) {
+					uuid += '-';
+				}
+				uuid += (i === 12 ? 4 : (i === 16 ? (random & 3 | 8) : random)).toString(16);
+			}
+
+			return uuid;
+		},
+		pluralize: function (count, word) {
+			return count === 1 ? word : word + 's';
+		},
+		store: function (namespace, data) {
+			if (arguments.length > 1) {
+				return localStorage.setItem(namespace, JSON.stringify(data));
+			} else {
+				var store = localStorage.getItem(namespace);
+				return (store && JSON.parse(store)) || [];
+			}
+		}
+	};
+
+	var App = {
+		init: function () {
+			this.todos = util.store('todos-jquery');
+			this.todoTemplate = Handlebars.compile($('#todo-template').html());
+			this.bindEvents();
+
+			new Router({
+				'/:filter': function (filter) {
+					this.filter = filter;
+					this.render();
+				}.bind(this)
+			}).init('/all');
+		},
+		bindEvents: function () {
+			$('#new-todo').on('keyup', this.create.bind(this));
+			$('#todo-list').on('click', 'a', this.destroy.bind(this));
+		},
+		render: function () {
+			var todos = this.todos;
+			$('#todo-list').html(this.todoTemplate(todos));
+			//$('#main').toggle(todos.length > 0);
+			//$('#toggle-all').prop('checked', this.getActiveTodos().length === 0);
+			$('#new-todo').focus();
+			util.store('todos-jquery', this.todos);
+		},
+		// accepts an element from inside the `.item` div and
+		// returns the corresponding index in the `todos` array
+		indexFromEl: function (el) {
+			var id = $(el).closest('li').data('id');
+			var todos = this.todos;
+			var i = todos.length;
+
+			while (i--) {
+				if (todos[i].id === id) {
+					return i;
+				}
+			}
+		},
+		create: function (e) {
+			var $input = $(e.target);
+			var val = $input.val().trim();
+
+			if (e.which !== ENTER_KEY || !val) {
+				return;
+			}
+
+			this.todos.push({
+				id: util.uuid(),
+				title: val
+			});
+
+			$input.val('');
+
+			this.render();
+		},
+		destroy: function (e) {
+			this.todos.splice(this.indexFromEl(e.target), 1);
+			this.render();
+		}
+	};
+
+	App.init();
+});
